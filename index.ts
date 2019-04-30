6ac967714d0ef40393503919541242f01d9ce42e
Mel Michael <mel.michael007@gmail.com>
2019-04-30 10:30:54 +0100

    Complete weather information display

--- index.ts
+++ index.ts
@@ -7,8 +7,27 @@ const API_KEY='b496510926f77f688cf2edfb07084e0f'
 
 const getWeatherByZipCode = async (postalCode: number) => {
   const URL = `${WEATHER_API}${postalCode},us&APPID=${API_KEY}` 
-	axios.get(URL)
-	 .then(data => console.log(data))
-	 .catch(err => console.error(err))
+	try {
+    return await axios.get(URL)
+  } catch (err) {
+    console.error(err)
+	}
 }
 
+const getWeather = async ([locationName, postalCode]: [string, number]) => {
+  const currentTime = new Date().toTimeString()
+	const currentWeather = await getWeatherByZipCode(postalCode)
+	
+	// console.log(currentTime, currentWeather, locationName)
+	if(!currentWeather) {
+		console.log('Something went wrong, please try again.') 
+		process.exit(1);
+	}
+
+  if (currentWeather && currentWeather.data) {
+		const { name, weather } = currentWeather.data 
+    console.log(`${currentTime}: There's a ${weather[weather.length - 1].description} in ${name}, ${locationName}`)
+	}
+}
+
+getWeather(['NYC', 10001])
