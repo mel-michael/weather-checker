bf87edccd1ed841e2a627d54898ec00a4dde9f90
Mel Michael <mel.michael007@gmail.com>
2019-04-30 10:57:31 +0100

    Add env file and refactor code

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
+// External Dependencies
 import axios from 'axios'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+// constants
 const WEATHER_API = 'http://api.openweathermap.org/data/2.5/weather?zip=' 
+const API_KEY = process.env.API_KEY
 
-const API_KEY='b496510926f77f688cf2edfb07084e0f' 
-
-
-const getWeatherByZipCode = async (postalCode: number) => {
-  const URL = `${WEATHER_API}${postalCode},us&APPID=${API_KEY}` 
+const getWeatherByPostalCode = async (postalCode: number) => {
+	const URL = `${WEATHER_API}${postalCode},us&APPID=${API_KEY}` 
 	try {
     return await axios.get(URL)
   } catch (err) {
@@ -15,19 +18,19 @@ const getWeatherByZipCode = async (postalCode: number) => {
 }
 
 const getWeather = async ([locationName, postalCode]: [string, number]) => {
-  const currentTime = new Date().toTimeString()
-	const currentWeather = await getWeatherByZipCode(postalCode)
+	const currentTime = new Date().toTimeString() 
+	const currentWeather = await getWeatherByPostalCode(postalCode) 
 	
-	// console.log(currentTime, currentWeather, locationName)
 	if(!currentWeather) {
-		console.log('Something went wrong, please try again.') 
-		process.exit(1);
+		console.log('Something went wrong, please try again.')  
+		process.exit(1) 
 	}
 
   if (currentWeather && currentWeather.data) {
 		const { name, weather } = currentWeather.data 
-    console.log(`${currentTime}: There's a ${weather[weather.length - 1].description} in ${name}, ${locationName}`)
+    console.log(`${currentTime}: There's a ${weather[weather.length - 1].description} in ${name}, ${locationName}`) 
 	}
 }
 
+// Call getWeather with test input
 getWeather(['NYC', 10001])
